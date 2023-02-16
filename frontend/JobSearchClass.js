4698a7dcb1db8791672075ca7d643b36faface31
TimmyChan99 <felemenoun@gmail.com>
2023-02-16 18:30:44 +0100

    Setup linters and fix errors

--- frontend/JobSearchClass.js
+++ frontend/JobSearchClass.js
@@ -1,50 +1,48 @@
-import { extractFormData, jobTemplate, errorTemplate, noJobsTemplate } from './utils';
+import {
+  extractFormData, jobTemplate, errorTemplate, noJobsTemplate,
+} from './utils.js';
 
 class JobSearchClass {
-	constructor(searchFormSelector, jobsListContainerSelector, loadinglementSelector) {
-		this.searchForm = document.querySelector(searchFormSelector);
-		this.jobsListContainer = document.querySelector(jobsListContainerSelector);
-		this.loading = document.querySelector(loadinglementSelector);
-	}
-	
-	// Start loading animation
-	showLoading () {
-		this.loading.classList.add('show');
-	}
+  constructor(searchFormSelector, jobsListContainerSelector, loadinglementSelector) {
+    this.searchForm = document.querySelector(searchFormSelector);
+    this.jobsListContainer = document.querySelector(jobsListContainerSelector);
+    this.loading = document.querySelector(loadinglementSelector);
+  }
 
-	// Stop loading animation
-	hideLoading () {
-		this.loading.classList.remove('show');
-	}
+  // Start loading animation
+  showLoading() {
+    this.loading.classList.add('show');
+  }
 
-	// Method: get the job data from the backend API
-	async getJobData () {
-		this.searchForm.addEventListener('submit', async (e) => {
-			e.preventDefault();
-			this.showLoading();
-			this.jobsListContainer.innerHTML = '';
-	
-			const { location, search, country } = extractFormData(this.searchForm);
-			try {
-				const jobData = await fetch(`http://localhost:3000/?country=${country}&search=${search}&location=${location}`);
-				const jobs = await jobData.json();
-				this.jobs = jobs.results;
-				this.hideLoading();
+  // Stop loading animation
+  hideLoading() {
+    this.loading.classList.remove('show');
+  }
 
-				if (this.jobs?.length === 0) {
-					noJobsTemplate();
-				} else {
-					this.jobsListContainer.innerHTML = this.jobs.map((job) => jobTemplate(job))
-					                                  .join('');
-				}
+  // Method: get the job data from the backend API
+  async getJobData() {
+    this.searchForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      this.showLoading();
+      this.jobsListContainer.innerHTML = '';
 
-				} catch (error) {
-					console.log(error);
-					this.hideLoading();
-				  errorTemplate('Something went wrong');
-				}
-	});
-}
+      const { location, search, country } = extractFormData(this.searchForm);
+      try {
+        const jobData = await fetch(`http://localhost:3000/?country=${country}&search=${search}&location=${location}`);
+        const jobs = await jobData.json();
+        this.jobs = jobs.results;
+        this.hideLoading();
 
+        if (this.jobs?.length === 0) {
+          noJobsTemplate();
+        } else {
+          this.jobsListContainer.innerHTML = this.jobs.map((job) => jobTemplate(job)).join('');
+        }
+      } catch (error) {
+        this.hideLoading();
+        errorTemplate('Something went wrong');
+      }
+    });
+  }
 }
 export default JobSearchClass;
